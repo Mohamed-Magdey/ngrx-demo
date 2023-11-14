780ec07dbcca15cb3d7ff481152190034bcb3198
Mohamed-Magdey <ragabmohame6@gmail.com>
2023-11-14 12:52:33 +0200

    Register new states

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
 
 @NgModule({
-  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  imports: [
+    BrowserModule, 
+    StoreModule.forRoot({ count: counterReducer, books: booksReducer, collection: collectionReducer }),
+    HttpClientModule
+  ],
   declarations: [AppComponent, MyCounterComponent, BookListComponent],
   bootstrap: [AppComponent],
 })
